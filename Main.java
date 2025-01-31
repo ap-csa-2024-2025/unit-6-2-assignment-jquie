9ab837b31d44fcff2b78a5c70c4ee8a7563fd4f9
jquie <jacquelynphan03@gmail.com>
2025-01-31 22:41:35 +0000

    done

--- Main.java
+++ Main.java
@@ -8,6 +8,8 @@ public class Main
     Scanner sc = new Scanner(System.in);
     System.out.println("Enter array length");
     int len = sc.nextInt();
+    System.out.println("Enter a value for num:");
+    int num = sc.nextInt();
 
     int[] arr = new int[len];
     System.out.println("Enter values");
@@ -16,11 +18,46 @@ public class Main
       arr[i] = sc.nextInt();
     }
     System.out.println("Contains negative: " + containsNeg(arr));
-  }
 
+    System.out.println("Number divisible by three: " + numDivisibleBy3(arr));
+
+    System.out.println("Num divisible by " + num + ": " + numDivisible(arr, num));
+  }
+  // Problem 1
   public static boolean containsNeg(int[] array)
   {
-    // Implement Problem 1 here
-    return 0;
+    for (int i = 0; i < array.length; i++)
+    {
+      if (!(array[i] > 0))
+      {
+        return true;
+      }
+    }
+    return false;
   }
+
+  //Problem 2
+  public static int numDivisibleBy3(int[] array){
+    int count = 0;
+    for (int i = 0; i < array.length; i++)
+    {
+      if ((array[i] % 3) == 0)
+      {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  //Problem 3
+  public static int numDivisible(int[] array, int num){
+    int count = 0;
+    for (int i = 0; i < array.length; i++){
+      if ((array[i] % num) == 0){
+        count++;
+      }
+    }
+    return count;
+  }
+
 }
